ef7c3230b776be75f58e9cc611822ebeb1b42405
Whitney Zoller <whitneyrzoller@gmail.com>
2014-03-08 19:49:45 -0500

    sort and panic

---
+++ panic.go
@@ -0,0 +1,28 @@
+package main
+
+import "os"
+
+// A panic typically means something went unexpectedly wrong. 
+// Mostly we use it to fail fast on errors that shouldn’t 
+// occur during normal operation, or that we aren’t prepared 
+// to handle gracefully, for example if a http server could not 
+// connect to some data resource (MySQL, redis, etc).
+func main() {
+  // We’ll use panic throughout this site to check for 
+  // unexpected errors. This is the only program on the site 
+  // designed to panic.
+  //panic("fml, wtf")
+
+  // A common use of panic is to abort if a function returns 
+  // an error value that we don’t know how to (or want to) handle. 
+  // Here’s an example of panicking if we get an unexpected error 
+  // when creating a new file.
+  _, err := os.Create("/tmp/file")
+  if err != nil {
+    panic(err)
+  }
+}
+
+// NOTE: Note that unlike some languages which use exceptions for 
+// handling of many errors, in Go it is idiomatic to use error-indicating 
+// return values wherever possible.
